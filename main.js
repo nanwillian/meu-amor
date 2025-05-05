e0ac443c723eb359d524153bded509ce5a219e01
renanwguimaraes <nan.guimawillian@gmail.com>
2025-05-05 16:02:53 -0300

    estilo rosa

--- main.js
+++ main.js
@@ -197,6 +197,14 @@ document.addEventListener('DOMContentLoaded', () => {
       image: "amigos/lais.jpeg"
     }
   ];
+
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+shuffle(friends);
   
   let currentFriend = 0;
   const bg = document.getElementById('friend-bg');
